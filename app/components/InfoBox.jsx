2eef801afd68706737c5c3c4cdf7447d488e8456
moedeera <82620264+moedeera@users.noreply.github.com>
2024-05-23 08:48:07 -0600

    committ

--- app/components/InfoBox.jsx
+++ app/components/InfoBox.jsx
@@ -3,15 +3,20 @@ const InfoBox = ({
   backgroundColor = "bg-gray-100",
   textColor = "text-gray-800",
   buttonInfo,
+  buttonColor = "black",
   children,
 }) => {
   return (
-    <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+    <div
+      className={` p-6 rounded-lg shadow-md`}
+      style={{ backgroundColor: backgroundColor }}
+    >
       <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
       <p className={`${textColor} mt-2 mb-4`}>{children}</p>
       <a
         href={buttonInfo.link}
-        className={`inline-block ${buttonInfo.backgroundColor} text-white rounded-lg px-4 py-2 hover:opacity-80`}
+        className={`inline-block  text-white rounded-lg px-4 py-2 hover:opacity-80`}
+        style={{ backgroundColor: buttonColor }}
       >
         {buttonInfo.text}
       </a>
--- app/components/InfoBoxes.jsx
+++ app/components/InfoBoxes.jsx
@@ -6,7 +6,7 @@ const InfoBoxes = () => {
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <InfoBox
-            heading="For Renters"
+            heading="Follow a league"
             backgroundColor="bg-gray-100"
             buttonInfo={{
               text: "Browse Properties",
@@ -18,13 +18,14 @@ const InfoBoxes = () => {
             owners.
           </InfoBox>
           <InfoBox
-            heading="For Property Owners"
+            heading="Start a league"
             backgroundColor="bg-blue-100"
             buttonInfo={{
               text: "Add Property",
               link: "/properties/add",
-              backgroundColor: "bg-blue-500",
+              backgroundColor: "#169873c0",
             }}
+            buttonColor="#169873"
           >
             List your properties and reach potential tenants. Rent short or long
             term.
