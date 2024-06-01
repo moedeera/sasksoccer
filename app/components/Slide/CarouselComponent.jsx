67b134c13ca022dc1b75c4fbe7a5f76065640821
moedeera <82620264+moedeera@users.noreply.github.com>
2024-05-31 18:25:48 -0600

    committ

--- app/components/Slide/CarouselComponent.jsx
+++ app/components/Slide/CarouselComponent.jsx
@@ -63,32 +63,34 @@ const CarouselComponent = ({ data }) => {
   }, []);
 
   return (
-    <div className="component-container">
-      <div className="text-3xl font-bold h3-header py-4">Latest</div>
-      <Carousel>
-        <CarouselContent>
-          {info.map((item, index) => (
-            <CarouselItem
-              key={index}
-              className="md:basis-1/2 lg:basis-1/4 py-3"
-            >
-              <div
-                className={`min-h-72 m-w-36 border-2 border-solid rounded flex flex-col justify-center px-3 pt-2 pb-4 bg-neutral-${
-                  index + 1
-                }00`}
+    <div className="component-container border-x-2 ">
+      <div className="carousel-container">
+        <div className="text-3xl font-bold h3-header py-4">Latest</div>
+        <Carousel className="">
+          <CarouselContent>
+            {info.map((item, index) => (
+              <CarouselItem
+                key={index}
+                className="md:basis-1/2 lg:basis-1/4 py-3"
               >
-                <div className="text-2xl font-bold h3-header px-0 pb-3">
-                  {item.title}
+                <div
+                  className={`min-h-72 m-w-36 border-2 border-solid rounded flex flex-col justify-center px-3 pt-2 pb-4 bg-neutral-${
+                    index + 1
+                  }00`}
+                >
+                  <div className="text-2xl font-bold h3-header px-0 pb-3">
+                    {item.title}
+                  </div>
+                  <small className="py-1">May 2{index * 2 + 1} 2024</small>
+                  <p className="pr-2 ">{item.content}</p>
                 </div>
-                <small className="py-1">May 2{index * 2 + 1} 2024</small>
-                <p className="pr-2 ">{item.content}</p>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 };
