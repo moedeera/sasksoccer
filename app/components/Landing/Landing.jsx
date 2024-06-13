bd658983b3ff59a098241692390b12cefc734e58
moedeera <82620264+moedeera@users.noreply.github.com>
2024-06-12 23:09:16 -0600

    committ

--- app/components/Landing/Landing.jsx
+++ app/components/Landing/Landing.jsx
@@ -6,12 +6,8 @@ const img = "./soccer.jpeg";
 
 const Landing = ({ data }) => {
   let defaultInfo = {
-    title: "Saskatoon Soccer News",
-    content: ` Take a tour of Canada's only synchrotron research facility, for
-    free! Take a tour of Canada's only synchrotron research facility,
-    for free! Take a tour of Canada's only synchrotron research
-    facility, for free! Take a tour of Canada's only synchrotron
-    research facility, for free!`,
+    title: "Currently Under Construction",
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum,`,
     button: true,
     buttonLink: "/",
     buttonName: "Read More",
