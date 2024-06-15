46c3dbef49194c056edba55bb85c3b163cbf86b8
moedeera <82620264+moedeera@users.noreply.github.com>
2024-06-14 20:41:29 -0600

    committ

--- app/components/FormComponent/FormComponent.jsx
+++ app/components/FormComponent/FormComponent.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 // Sample components from your UI library (replace with actual imports)
 import { Input } from "@/components/ui/input";
