29d2b711914ff149de97d0face33171bf70d83a0
moedeera <82620264+moedeera@users.noreply.github.com>
2024-07-25 04:47:59 -0600

    committ

--- app/leagues/[slug]/info.js
+++ app/leagues/[slug]/info.js
@@ -1,7 +1,7 @@
 const errorReportInfo = {
   title: "Error in the data above?",
 
-  content: `If there is any error in the data above please contact the owner of the Administrator of the league
+  content: `If there is any error in the data above please contact the Administrator of the league
        `,
   button: true,
   buttonText: "Contact Admin",
