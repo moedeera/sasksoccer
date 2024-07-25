4580be3a3716e18f601fbd863614a597a23324a8
moedeera <82620264+moedeera@users.noreply.github.com>
2024-07-25 03:56:56 -0600

    committ

--- app/components/LeagueUpdateForm/EditTeams.jsx
+++ app/components/LeagueUpdateForm/EditTeams.jsx
@@ -3,11 +3,28 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React, { useState } from "react";
 import Spinner from "../Spinner";
+import { generateId } from "@/app/utlils/functions";
+generateId;
 
 const EditTeams = ({ teams, setTeams, league, error, setError }) => {
   const [editTeams, setEditTeams] = useState("");
   const [editTeamsGroup, setEditTeamsGroup] = useState("");
   const [loading, setLoading] = useState(false);
+  const [addTeam, showAddTeam] = useState(false);
+  const [newTeam, setNewTeam] = useState({
+    id: null,
+    name: "",
+    win_total: 0,
+    loss_total: 0,
+    draw_total: 0,
+    goals_for: 0,
+    goals_against: 0,
+    color1: "gray",
+    color2: "white",
+    coach1: null,
+    coach2: null,
+    group: "Group A",
+  });
 
   const handleEditTeam = (value, name) => {
     const updatedTeams = teams.map((team) =>
@@ -15,6 +32,14 @@ const EditTeams = ({ teams, setTeams, league, error, setError }) => {
     );
     setTeams(updatedTeams);
     setEditTeams(value);
+  };
+
+  const handleEditTeamStat = (stat, value, name) => {
+    const updatedTeams = teams.map((team) =>
+      team.name === name ? { ...team, [stat]: value } : team
+    );
+    setTeams(updatedTeams);
+    setEditTeamsGroup(value);
   };
 
   const handleEditTeamGroup = (value, name) => {
@@ -75,25 +100,112 @@ const EditTeams = ({ teams, setTeams, league, error, setError }) => {
           <div className="flex justify-between" key={index}>
             {" "}
             {editTeams === team.name ? (
-              <>
-                {" "}
-                <Input
-                  type="text"
-                  value={team.name}
-                  onChange={(e) => handleEditTeam(e.target.value, team.name)}
-                  className="mr-8"
-                />
-                {league.groups && (
+              <div>
+                <div className="mb-2">
+                  {" "}
                   <Input
                     type="text"
-                    value={team.group}
-                    onChange={(e) =>
-                      handleEditTeamGroup(e.target.value, team.name)
-                    }
-                    className="mr-8"
+                    value={team.name}
+                    onChange={(e) => handleEditTeam(e.target.value, team.name)}
+                    className="mr-8 mb-2 max-w-80"
                   />
-                )}
-              </>
+                  {league.groups && (
+                    <Input
+                      type="text"
+                      value={team.group}
+                      onChange={(e) =>
+                        handleEditTeamGroup(e.target.value, team.name)
+                      }
+                      className="mr-8 max-w-80"
+                    />
+                  )}
+                </div>
+                <>
+                  <div className="bg-gray-200 p-3 mt-2">
+                    {
+                      <>
+                        <div className="flex gap-2 items-center mt-2 flex-wrap">
+                          <div className="flex gap-2 items-center max-w-40">
+                            <label>Wins</label>{" "}
+                            <Input
+                              type="number"
+                              value={team.win_total}
+                              onChange={(e) =>
+                                handleEditTeamStat(
+                                  "win_total",
+                                  e.target.value,
+                                  team.name
+                                )
+                              }
+                              className="mr-8 max-w-60"
+                            />
+                          </div>
+                          <div className="flex gap-2 items-center max-w-40">
+                            <label>Lose</label>{" "}
+                            <Input
+                              type="number"
+                              value={team.loss_total}
+                              onChange={(e) =>
+                                handleEditTeamStat(
+                                  "loss_total",
+                                  e.target.value,
+                                  team.name
+                                )
+                              }
+                              className="mr-8 max-w-60"
+                            />
+                          </div>
+                          <div className="flex gap-2 items-center max-w-40">
+                            <label>Draw</label>{" "}
+                            <Input
+                              type="number"
+                              value={team.draw_total}
+                              onChange={(e) =>
+                                handleEditTeamStat(
+                                  "draw_total",
+                                  e.target.value,
+                                  team.name
+                                )
+                              }
+                              className="mr-8 max-w-60"
+                            />
+                          </div>
+                          <div className="flex gap-2 items-center max-w-40">
+                            <label>Goals</label>{" "}
+                            <Input
+                              type="number"
+                              value={team.goals_for}
+                              onChange={(e) =>
+                                handleEditTeamStat(
+                                  "goals_for",
+                                  e.target.value,
+                                  team.name
+                                )
+                              }
+                              className="mr-8 max-w-60"
+                            />
+                          </div>
+                          <div className="flex gap-2 items-center max-w-40">
+                            <label>GA</label>{" "}
+                            <Input
+                              type="number"
+                              value={team.goals_against}
+                              onChange={(e) =>
+                                handleEditTeamStat(
+                                  "goals_against",
+                                  e.target.value,
+                                  team.name
+                                )
+                              }
+                              className="mr-8 max-w-60"
+                            />
+                          </div>
+                        </div>
+                      </>
+                    }
+                  </div>
+                </>
+              </div>
             ) : (
               <>
                 {" "}
@@ -144,6 +256,125 @@ const EditTeams = ({ teams, setTeams, league, error, setError }) => {
             </div>
           </div>
         ))}
+      </div>
+      <div className="bg-gray-200 p-3 mt-2">
+        {addTeam && (
+          <>
+            <div className="flex gap-3 items-center">
+              <label className="font-bold">Name</label>
+              <Input
+                type="text"
+                value={newTeam.name}
+                onChange={(e) =>
+                  setNewTeam({ ...newTeam, name: e.target.value })
+                }
+                className="mr-8 max-w-60"
+              />
+              {league.groups && (
+                <>
+                  {" "}
+                  <label className="font-bold">Group</label>
+                  <Input
+                    type="text"
+                    value={newTeam.group}
+                    onChange={(e) =>
+                      setNewTeam({ ...newTeam, group: e.target.value })
+                    }
+                    className="mr-8 max-w-60"
+                  />
+                </>
+              )}
+            </div>
+            <div className="flex gap-2 items-center mt-2 flex-wrap">
+              <div className="flex gap-2 items-center max-w-40">
+                <label>Wins</label>{" "}
+                <Input
+                  type="number"
+                  value={newTeam.win_total}
+                  onChange={(e) =>
+                    setNewTeam({ ...newTeam, win_total: e.target.value })
+                  }
+                  className="mr-8 max-w-60"
+                />
+              </div>
+              <div className="flex gap-2 items-center max-w-40">
+                <label>Lose</label>{" "}
+                <Input
+                  type="number"
+                  value={newTeam.loss_total}
+                  onChange={(e) =>
+                    setNewTeam({ ...newTeam, loss_total: e.target.value })
+                  }
+                  className="mr-8 max-w-60"
+                />
+              </div>
+              <div className="flex gap-2 items-center max-w-40">
+                <label>Draw</label>{" "}
+                <Input
+                  type="number"
+                  value={newTeam.draw_total}
+                  onChange={(e) =>
+                    setNewTeam({ ...newTeam, draw_total: e.target.value })
+                  }
+                  className="mr-8 max-w-60"
+                />
+              </div>
+              <div className="flex gap-2 items-center max-w-40">
+                <label>Goals</label>{" "}
+                <Input
+                  type="number"
+                  value={newTeam.goals_for}
+                  onChange={(e) =>
+                    setNewTeam({ ...newTeam, goals_for: e.target.value })
+                  }
+                  className="mr-8 max-w-60"
+                />
+              </div>
+              <div className="flex gap-2 items-center max-w-40">
+                <label>GA</label>{" "}
+                <Input
+                  type="number"
+                  value={newTeam.goals_against}
+                  onChange={(e) =>
+                    setNewTeam({ ...newTeam, goals_against: e.target.value })
+                  }
+                  className="mr-8 max-w-60"
+                />
+              </div>
+            </div>
+          </>
+        )}
+        <div className="flex gap-3 mt-3">
+          {" "}
+          <Button
+            variant={addTeam ? "destructive" : "success"}
+            onClick={() => {
+              showAddTeam(!addTeam);
+            }}
+          >
+            {addTeam ? "Cancel" : "Add Team"}
+          </Button>
+          {addTeam && (
+            <Button
+              variant={"success"}
+              onClick={() => {
+                const newlyAddedTeam = {
+                  ...newTeam,
+                  id: generateId(newTeam.name),
+                };
+                const updatedLeaguePreview = {
+                  ...league,
+                  teams: [...league.teams, newlyAddedTeam],
+                };
+                const updatedTeams = [...teams, newlyAddedTeam];
+                setTeams(updatedTeams);
+                console.log(updatedLeaguePreview);
+              }}
+            >
+              Add
+            </Button>
+          )}
+        </div>
       </div>
     </>
   );
--- app/leagues/[slug]/info.js
+++ app/leagues/[slug]/info.js
@@ -1,10 +1,10 @@
 const errorReportInfo = {
   title: "Error in the data above?",
 
-  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum,
+  content: `If there is any error in the data above please contact the owner of the Administrator of the league
        `,
   button: true,
-  buttonText: "Contact Manager",
+  buttonText: "Contact Admin",
   buttonLink: "/",
 };
 
