909a11e16d83c822aa5fdb576525b0d545bfd99b
Moe Deera <deeramoe@gmail.com>
2024-08-13 12:54:37 -0600

    committ

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -10,49 +10,80 @@ const FooterComponent = () => {
   return (
     <footer className="bg-black text-white py-8">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-        <div className="mb-4 md:mb-0">
+        <div className="mb-4 md:mb-0 mt-4">
           <Link href={"/"}>
-            <Image className="" src={logo} alt="" width={200} height={200} />
+            <Image className="" src={logo} alt="" width={300} height={300} />
           </Link>
           <p className="text-sm mt-2 max-w-80">
             <div>
               Dedicated to connecting soccer enthusiast in and around Saskatoon.
-              Interested in keeping up to date with latest news and updates?.
-              Sign up for our weekly newsletter.
+              For more content and latest updates, follow us on social media{" "}
             </div>
-            <Input className="my-3" placeholder="Enter in your email" />
-            <button className="btn-carousel">Sign Up</button>
+            <div className="flex space-x-4 mt-4 md:h-44">
+              <Button variant="ghost" as="a" href="https://facebook.com">
+                <FaFacebook className="w-5 h-5" />
+              </Button>
+              <Button variant="ghost" as="a" href="https://twitter.com">
+                <FaTwitter className="w-5 h-5" />
+              </Button>
+              <Button variant="ghost" as="a" href="https://instagram.com">
+                <FaInstagram className="w-5 h-5" />
+              </Button>
+              <Button variant="ghost" as="a" href="https://linkedin.com">
+                <FaLinkedin className="w-5 h-5" />
+              </Button>
+            </div>
           </p>
         </div>
-        <div className="mb-4 md:mt-0 h-44">
-          <nav className="hidden md:flex  md:flex-column space-y-2 md:space-y-0 md:space-x-4">
-            <a href="/" className="hover:underline">
-              Home
-            </a>
-            <a href="/about" className="hover:underline">
-              About Us
-            </a>
-            <a href="/latest" className="hover:underline">
-              Latest
-            </a>
-            <a href="/contact" className="hover:underline">
-              Contact
-            </a>
-          </nav>
+        <div className="hidden md:flex flex-col w-full md:w-1/4 px-4 h-80">
+          <div
+            className="mb-3 w-1/4"
+            style={{ borderBottom: "3px solid gray" }}
+          >
+            {" "}
+            Links
+          </div>
+          <div className="flex gap-20 w-full">
+            {" "}
+            <nav className="flex flex-col gap-y-4 text-sm text-slate-400">
+              <a href="/" className="hover:underline">
+                Home
+              </a>
+              <a href="/about" className="hover:underline">
+                About Us
+              </a>
+              <a href="/latest" className="hover:underline">
+                Latest
+              </a>
+              <a href="/contact" className="hover:underline">
+                Contact
+              </a>
+            </nav>
+            <nav className="flex flex-col gap-y-4 text-sm text-slate-400">
+              <a href="/" className="hover:underline">
+                Community
+              </a>
+              <a href="/about" className="hover:underline">
+                Privacy Policy
+              </a>
+              <a href="/latest" className="hover:underline">
+                Careers
+              </a>
+              <a href="/contact" className="hover:underline">
+                Disclaimer
+              </a>
+            </nav>
+          </div>
         </div>
-        <div className="flex space-x-4 mt-0 md:h-44">
-          <Button variant="ghost" as="a" href="https://facebook.com">
-            <FaFacebook className="w-5 h-5" />
-          </Button>
-          <Button variant="ghost" as="a" href="https://twitter.com">
-            <FaTwitter className="w-5 h-5" />
-          </Button>
-          <Button variant="ghost" as="a" href="https://instagram.com">
-            <FaInstagram className="w-5 h-5" />
-          </Button>
-          <Button variant="ghost" as="a" href="https://linkedin.com">
-            <FaLinkedin className="w-5 h-5" />
-          </Button>
+        <div className="w-1/4">
+          {" "}
+          <div>
+            {" "}
+            Interested in keeping up to date with latest news and updates?. Sign
+            up for our weekly newsletter.
+          </div>
+          <Input className="my-3" placeholder="Enter in your email" />
+          <button className="btn">Sign Up</button>
         </div>
       </div>
       <div className="text-center mt-8 text-sm text-gray-400">
--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import Hero from "./components/Hero";
-import InfoBoxes from "./components/InfoBoxes";
 
 import { Cards } from "@/components/Card/Card";
 import Landing from "./components/Landing/Landing";
 import Block1 from "./components/Block1/Block1";
-import Calendar from "./components/Calendar/Calendar";
-import CarouselComponent from "./components/Slide/CarouselComponent";
-import TableComponent from "./components/Table/TableComponent";
 
-import LeagueForm from "./components/FormComponent/FormComponent";
-import LoginFormComponent from "./components/LoginFormComponent/LoginFormComponent";
-import RegisterForm from "./components/RegisterForm/RegisterForm";
+import CarouselComponent from "./components/Slide/CarouselComponent";
+
 import Block2 from "./components/Block2/Block2";
-import Block3 from "./components/Block3/Block3";
-import Block4 from "./components/Block4/Block4";
+
 import { homepageInfo } from "./homepagecontent";
 import Banner from "./components/Banner/Banner";
 
