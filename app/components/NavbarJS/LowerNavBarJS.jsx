d3210926d804340add865cfd3c55372ccf091866
Moe Deera <deeramoe@gmail.com>
2024-10-29 22:06:57 -0600

    committ

--- app/components/NavbarJS/LowerNavBarJS.jsx
+++ app/components/NavbarJS/LowerNavBarJS.jsx
@@ -5,30 +5,43 @@ import { GlobalContext } from "@/app/context/GlobalContext";
 import Link from "next/link";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { BsFillCaretDownFill } from "react-icons/bs";
-import { Input } from "@/components/ui/input";
 
 const LowerNavBarJS = () => {
   const { headerLinks } = useContext(GlobalContext);
-  const { selected, setSelected } = useState(null);
+  const [selected, setSelected] = useState(null);
 
   return (
     <div className="mb-lower-navbar-container">
       <div className="mb-lower-navbar">
         <div className="mb-lower-navbar-links">
-          <Link href={"/"}>Home</Link>{" "}
+          <Link
+            href={"/"}
+            onClick={() => {
+              setSelected(null);
+            }}
+          >
+            Home
+          </Link>{" "}
           {headerLinks.map(
             (link, index) =>
               index > 0 && (
                 <div
                   onClick={() => {
                     setSelected(link.name);
+                    console.log("testing:", link.name, selected);
                   }}
                   key={index}
                   href={link.Link}
-                  className="mb-links"
+                  className={
+                    selected === link.name
+                      ? "mb-links selected-link"
+                      : "mb-links"
+                  }
                 >
                   {link.name}
-                  <BsFillCaretDownFill style={{ fontSize: "10px" }} />
+                  <BsFillCaretDownFill
+                    style={{ fontSize: "10px", marginLeft: "3px" }}
+                  />
                   <div
                     className={
                       selected === link.name && "mb-lower-navbar-dropdown"
