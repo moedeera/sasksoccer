01a0c0f58c2048e37ace631fb70d2d6317de0b82
Moe Deera <deeramoe@gmail.com>
2024-12-12 23:46:47 -0600

    search page
    search page started

--- app/search/[keyword]/page.jsx
+++ app/search/[keyword]/page.jsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import React, { useEffect } from "react";
 
 const Page = () => {
+  const { keyword } = useParams();
   useEffect(() => {
     console.log("hello");
   }, []);
   return (
     <div className="component-container">
-      <div className="text-xl">Search</div>
+      <div className="">
+        Search results for{" "}
+        <span className="font-bold text-cyan-600">{keyword}</span>{" "}
+      </div>
     </div>
   );
 };
