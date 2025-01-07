1228f1f1ed9c37ffd862b7721d08db75f483fd8f
Moe Deera <deeramoe@gmail.com>
2025-01-07 04:33:31 -0600

    Navbar updated
    Navbar Updated

--- app/components/NavbarJS/NavbarJS.jsx
+++ app/components/NavbarJS/NavbarJS.jsx
@@ -28,19 +28,48 @@ const NavbarJS = () => {
         >
           X
         </div>
-        <div className="pt-4 flex flex-col justify-evenly gap-8 text-lg font-bold">
-          {" "}
-          {headerLinks.map((page, index) => (
-            <Link
-              onClick={() => {
-                setSideMenu(false);
-              }}
-              key={index}
-              href={page.Link}
-            >
-              {page.name}
-            </Link>
-          ))}
+        <div className="grid grid-cols-2  mt-12 text-sm ">
+          <div className="pt-4 flex flex-col  gap-8 ">
+            {" "}
+            <div className=" pl-2 font-bold">
+              Directories{" "}
+              <span className="text-gray-400 font-bold text-lg"> | </span>
+            </div>
+            {headerLinks.map((page, index) => (
+              <Link
+                onClick={() => {
+                  setSideMenu(false);
+                }}
+                className="pl-2 font-semibold"
+                key={index}
+                href={page.Link}
+              >
+                {page.name}
+              </Link>
+            ))}
+          </div>
+          <div
+            className="pt-4 flex flex-col  gap-8 "
+            style={{ borderLeft: "1px solid rgba(128,128,128,0.5)" }}
+          >
+            {" "}
+            <div className=" pl-2 font-bold">
+              Leagues{" "}
+              <span className="text-gray-400 font-bold text-lg"> | </span>
+            </div>
+            {headerLinks[1]?.subLinks.map((league, index) => (
+              <Link
+                onClick={() => {
+                  setSideMenu(false);
+                }}
+                className=" pl-3 font-semibold"
+                key={index}
+                href={league.link}
+              >
+                {league.name}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
