5ea084f4beef221a4e36d5d46bae57fb7d750ab4
Moe Deera <deeramoe@gmail.com>
2025-03-12 00:41:41 -0600

    Blog setup
    Blog 2/3 of the way to first draft

--- app/components/BlogLanding/BlogLanding.jsx
+++ app/components/BlogLanding/BlogLanding.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./BlogLanding.css";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const BlogLanding = () => {
   const events = [
@@ -20,24 +22,32 @@ const BlogLanding = () => {
             A short description of the article goes here. This text sits near
             the bottom half of the image.
           </p>
-          <button>Read More</button>
+          <button className="btn">Read More</button>
         </div>
       </div>
 
       {/* Side container that has two items. 
           They’re side by side below 768px and stacked from 768px up. */}
       <div className="blog-header-side">
-        <div className="blog-header-side-item">
-          <div>
-            <h3>Upcoming Events</h3>
-            <div>
+        <Card className="blog-header-side-item flex flex-col space-in-between pt-4">
+          <CardContent>
+            <div className="text-lg font-bold mb-2">Upcoming Events</div>
+            <div className="flex flex-col gap-3 mb-2">
               {events.map((event) => (
-                <div key={event.id}>{event.name}</div>
+                <div key={event.id} className="flex flex-col">
+                  <div className="text-xs md:text-sm lg:text-md">
+                    {event.name}
+                  </div>
+                  <div className="hidden md:block text-xs">
+                    April 5 2025 @ 9:30 pm
+                  </div>
+                </div>
               ))}
             </div>
-          </div>
-        </div>
-        <div className="blog-header-side-item">Side Item 2</div>
+            <Button className="mt-2">See All</Button>
+          </CardContent>
+        </Card>
+        <div className="blog-header-side-item hidden md:block">Side Item 2</div>
       </div>
     </div>
   );
