25c1b20df6a7cd7e3ec7f9e0ed02d512a6c15e2a
Moe Deera <deeramoe@gmail.com>
2025-04-26 20:28:45 -0600

    minor fix
    minor fix

--- app/leagues/create/page.jsx
+++ app/leagues/create/page.jsx
@@ -4,7 +4,7 @@ import Landing from "../../components/Landing/Landing";
 
 import React from "react";
 
-export const page = () => {
+export const Page = () => {
   const openerContent = {
     title: `Add your current league`,
     content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum,
@@ -19,4 +19,4 @@ export const page = () => {
   );
 };
 
-export default page;
+export default Page;
