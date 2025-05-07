6e7b21477bf4d6f5447cd0781bd250760e597c2f
Moe Deera <deeramoe@gmail.com>
2025-05-07 01:26:22 -0600

    committ
    committ

--- app/leagues/OutdoorSeasonInfo.jsx
+++ app/leagues/OutdoorSeasonInfo.jsx
@@ -209,7 +209,7 @@ const OutdoorSeasonInfo = () => {
         {accordionSections.map(({ key, label }) => {
           const divisionData = teams[key];
           return (
-            <AccordionItem key={key} value={key} className="border bg-white">
+            <AccordionItem key={key} value={key} className=" bg-white">
               <AccordionTrigger className="hover:bg-emerald-500">
                 <span className="text-black group-hover:text-white">
                   {label}
